02f6774d4f054579caa9fc905e8cb64f2ac95d00
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-10-23 06:31:49 +0530

    Update NavBar.jsx

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -143,7 +143,7 @@ const NavBar = ({user,setUser,userInfo,nav,setNav}) => {
                 <input type="text" placeholder='Your Name'/><br/>
                 <input type="mail" onChange={(e)=> setUserName(e.target.value)} placeholder='Your mail' required/><br/>
                 <input type="password" onChange={(e)=> setPassword(e.target.value)} placeholder='Your Password' required/><br/>
-                <input type='checkbox' style={{color:"grey"}}/> I agree with terms and privacy policy
+                <input type='checkbox'/><span style={{color:"grey"}}> I agree with terms and privacy policy</span>
                 {load?<input type='submit' value={'wait...'}/>:<input type='submit' onClick={RegSub} value={'Register'}/>}
             </div>
 
